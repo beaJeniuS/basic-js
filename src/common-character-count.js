ea0a06464f75005031abe67bdaae1586e7230aa8
beaJenius <beajenius@yandex.ru>
2022-04-29 19:49:40 +0300

    feat: solve this problem

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  //throw new NotImplementedError('Not implemented');
+  let arr1 = s1.split('')
+  let arr2 = s2.split('')
+  let obj1 = {}
+  let obj2 = {}
+
+  for(let i=0;i<arr1.length;i++) {
+    if(obj1.hasOwnProperty(arr1[i])) {
+      obj1[arr1[i]] += 1
+    } else {
+      obj1[arr1[i]] = 1
+    }
+  }
+
+  for(let i=0;i<arr2.length;i++) {
+    if(obj1.hasOwnProperty(arr2[i])) {
+      if(obj2.hasOwnProperty(arr2[i])) {
+        obj2[arr2[i]] += 1
+      } else {
+        obj2[arr2[i]] = 1
+      }
+    }
+  }
+
+  let sum = 0
+  for(key in obj2) {
+    sum += Math.min(obj1[key], obj2[key])
+  }
+  return sum
 }
 
 module.exports = {
